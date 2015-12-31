fbb6f06129e0c09231945d9f2eba1d034b738cc9
Koss <koss@fortiss.org>
2015-12-31 17:52:01 +0100

    mas feliz

--- MasFeliz/MasFeliz/ViewController.swift
+++ MasFeliz/MasFeliz/ViewController.swift
@@ -8,8 +8,21 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensajePositivo: UILabel!
+    
+    let colores = Colores()
+    let frases = Datos()
+    
+    @IBAction func dameUnMensajePositivo() {
+        mensajePositivo.text = frases.regresaFrase()
+        //print("Esto sera muy positivo")
+        let colorAleatorio = colores.regresaColorAleatorio()
+        view.backgroundColor = colorAleatorio
+        view.tintColor = colorAleatorio
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
